88368b7c0ff457ea94e982f63d7b011b3eb144ec
roma-dev-expert <roma.k.devexpert@gmail.com>
2023-08-14 17:41:24 +0300

    refactoring: split code into functions, improve readability

--- Program.cs
+++ Program.cs
@@ -24,40 +24,44 @@ namespace ProxyServer
 
         static async Task HandleClientAsync(TcpClient client, string targetSite)
         {
-            using (var clientStream = client.GetStream())
+            using var clientStream = client.GetStream();
+            using var reader = new StreamReader(clientStream);
+            using var writer = new StreamWriter(clientStream);
+
+            var requestLine = await reader.ReadLineAsync();
+            var parts = requestLine?.Split(' ');
+
+            if (parts?.Length >= 3)
             {
-                using (var reader = new StreamReader(clientStream))
-                using (var writer = new StreamWriter(clientStream))
+                var method = parts[0];
+                var url = parts[1] == "/" ? "/ru" : parts[1];
+
+                if (method == "GET")
                 {
-                    var requestLine = await reader.ReadLineAsync();
-                    var parts = requestLine?.Split(' ');
+                    using var httpClient = new HttpClient();
+                    var response = await httpClient.GetAsync($"{targetSite}{url}");
 
-                    if (parts?.Length >= 3)
-                    {
-                        var method = parts[0];
-                        var url = parts[1] == "/" ? "/ru" : parts[1];
+                    var contentType = response.Content.Headers.ContentType?.MediaType;
+                    var isTextHtml = contentType != null && contentType.Contains("text/html");
 
-                        if (method == "GET")
-                        {
-                            using var httpClient = new HttpClient();
-                            var response = await httpClient.GetAsync($"{targetSite}{url}");
-
-                            var contentType = response.Content.Headers.ContentType?.MediaType;
-                            var isTextHtml = contentType != null && contentType.Contains("text/html");
-
-                            var modifiedContent = await ModifyContentAsync(response, isTextHtml);
-                            await writer.WriteLineAsync("HTTP/1.1 200 OK");
-                            await writer.WriteLineAsync($"Content-Length: {modifiedContent.Length}");
-                            await writer.WriteLineAsync("Connection: close");
-                            await writer.WriteLineAsync($"Content-type: {contentType}");
-                            await writer.WriteLineAsync();
-                            await writer.WriteAsync(modifiedContent);
-                        }
-                    }
+                    var modifiedContent = await ModifyContentAsync(response, isTextHtml);
+                    await WriteResponseAsync(writer, modifiedContent, contentType);
                 }
             }
 
             client.Close();
+        }
+
+        static bool IsInsideScriptTag(HtmlNode node)
+        {
+            while (node != null)
+            {
+                if (node.Name.Equals("script", StringComparison.OrdinalIgnoreCase)) return true;
+
+                node = node.ParentNode;
+            }
+
+            return false;
         }
 
         static async Task<string> ModifyContentAsync(HttpResponseMessage response, bool isTextHtml)
@@ -71,10 +75,8 @@ namespace ProxyServer
 
                 foreach (var textNode in doc.DocumentNode.DescendantsAndSelf())
                 {
-                    if (textNode.NodeType == HtmlNodeType.Text)
+                    if (textNode.NodeType == HtmlNodeType.Text && !IsInsideScriptTag(textNode))
                     {
-                        if (IsInsideScriptTag(textNode)) continue;
-
                         var modifiedText = Regex.Replace(textNode.InnerText, @"\b\w{6}\b", match =>
                         {
                             return match.Value + "™";
@@ -90,16 +92,14 @@ namespace ProxyServer
             return content;
         }
 
-        static bool IsInsideScriptTag(HtmlNode node)
+        static async Task WriteResponseAsync(StreamWriter writer, string content, string contentType)
         {
-            while (node != null)
-            {
-                if (node.Name.Equals("script", StringComparison.OrdinalIgnoreCase)) return true;
-
-                node = node.ParentNode;
-            }
-
-            return false;
+            await writer.WriteLineAsync("HTTP/1.1 200 OK");
+            await writer.WriteLineAsync($"Content-Length: {content.Length}");
+            await writer.WriteLineAsync("Connection: close");
+            await writer.WriteLineAsync($"Content-type: {contentType}");
+            await writer.WriteLineAsync();
+            await writer.WriteAsync(content);
         }
     }
 }
